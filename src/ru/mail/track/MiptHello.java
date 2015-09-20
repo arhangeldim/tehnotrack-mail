cc3981c466ccfd9f0fc7bdff241ed17cf04dd5bf
Dmitry Arkhangelskiy <arhangeldim@gmail.com>
2015-09-20 14:46:22 +0300

    Initial simple app

---
+++ src/ru/mail/track/MiptHello.java
@@ -0,0 +1,11 @@
+package ru.mail.track;
+
+/**
+ * Tehnotrack
+ * Simple console application.
+ */
+public class MiptHello {
+    public static void main(String[] args) {
+        System.out.println("Hello MIPT");
+    }
+}
